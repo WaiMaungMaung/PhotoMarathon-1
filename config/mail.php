8c5302d391e6ff9af46bec19bc50526e2b405b44
WaiMaungMaung <mgwaimaungmaung@gmail.com>
2020-12-22 09:41:39 +0630

    with office mail

--- config/mail.php
+++ config/mail.php
@@ -84,7 +84,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'idiotisme2@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'waimaungmaung@myanmargoldenrock.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
