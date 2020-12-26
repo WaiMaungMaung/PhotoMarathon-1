b3a177c6e4d802dacfee07d6ab9208e7dd155e13
www.myoaggarkyaw@gmail.com <69780253+eieilwin77442@users.noreply.github.com>
2020-12-26 12:31:38 +0630

    comit 20201226

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.master')
 
 @section('content')
@@ -8,18 +7,31 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="{{url('/img/wel1.jpg')}}" alt="First slide">
+      <img class="d-block w-100" src="{{url('/img/mr1.jpg')}}" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="{{url('/img/wel2.jpg')}}" alt="Second slide">
+      <img class="d-block w-100" src="{{url('/img/mr2.jpg')}}" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="{{url('/img/wel3.jpg')}}" alt="Third slide">
+      <img class="d-block w-100" src="{{url('/img/mr3.jpg')}}" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{url('/img/mr4.jpg')}}" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{url('/img/mr5.jpg')}}" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{url('/img/mr6.jpg')}}" alt="Third slide">
     </div>
   </div>
+  
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -32,7 +44,4 @@
 </div>
 @stop
 @section('content')
-    <div>
-        hello welcome
-    </div>
 @stop
