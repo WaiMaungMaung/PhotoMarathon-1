a2946bf8d52f5ccda675779d15a63c8d28829067
WaiMaungMaung <mgwaimaungmaung@gmail.com>
2021-01-11 10:27:41 +0630

    update

--- public/js/app.js
+++ public/js/app.js
@@ -52104,8 +52104,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\MGR\Desktop\30.Dec\PhotoMarathon\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\MGR\Desktop\30.Dec\PhotoMarathon\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\MGR\Desktop\Jan\PhotoMarathon\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\MGR\Desktop\Jan\PhotoMarathon\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
