280c7c5b6115bdfd0abc040dd9feaf21247c4d4d
www.myoaggarkyaw@gmail.com <69780253+eieilwin77442@users.noreply.github.com>
2021-01-14 15:09:02 +0630

    img name change

--- app/Http/Controllers/PsubmitController.php
+++ app/Http/Controllers/PsubmitController.php
@@ -85,38 +85,31 @@ class PsubmitController extends Controller
             $cBrand = $obj_enroll->camera_brand;
             if(request()->hasFile('image')){                
                 $img=request()->file('image')->getClientOriginalName();
-                request()->file('image')->move(public_path($themeCAT),$cmp.$cBrand.$img,'');            
+                request()->file('image')->move(public_path($themeCAT),$cmp.'_'.$cBrand.'_'.$img,'');            
             }
-            // to control submission time 
-            $isValidTime = true;
             date_default_timezone_set('Asia/Rangoon'); 
             $submitTime = date('d M Y h:i:s A');
             $status="";
-            
-            if($isValidTime){
-                $data=array('name'=>$name,'themeCAT'=>$themeCAT,'orgfileName'=>$img,'submitTime'=>$submitTime,'id'=>$id);
-                $status="success";
-                $msg = "Submission Success. You will recieve successful submission email.";
-                submission::create([
-                    'cmp'=>$cmp,
-                    'name'=>$name,
-                    'themeCAT'=>$themeCAT,
-                    'fileName'=>$cmp.$cBrand.$img,
-                    'submitTime'=>now(),
-                ]);
-                Mail::send('submitMail', $data, function($message) {                
-                    $message->to('myatthuaung@myanmargoldenrock.com', 'MMM')->subject
-                    ('Submission Successful!');
-                    $message->from(env('MAIL_FROM_ADDRESS'),'Cannon Photo Marathon');
-                });
-            }else{
-                $status="error";
-                $msg = "Sorry, Photo submission time is over";                
-            }   
+            $data=array('name'=>$name,'themeCAT'=>$themeCAT,'orgfileName'=>$img,'submitTime'=>$submitTime,'id'=>$id);
+            $status="success";
+            $msg = "Submission Success. You will recieve successful submission email.";
+            submission::create([
+                'cmp'=>$cmp,
+                'name'=>$name,
+                'themeCAT'=>$themeCAT,
+                'fileName'=>$cmp.'_'.$cBrand.'_'.$img,
+                'submitTime'=>now(),
+            ]);
+            Mail::send('submitMail', $data, function($message) {                
+                $message->to(Auth::user()->email, Auth::user()->name)->subject
+                ('Submission Successful!');
+                $message->from(env('MAIL_FROM_ADDRESS'),'Cannon Photo Marathon');
+            });
+
             return redirect()->route('submission')
                             ->with($status,$msg);
-            }else{
-                return redirect()->route('dashboard');
-            }        
-        }
+        }else{
+            return redirect()->route('dashboard');
+        }        
+    }
 }
